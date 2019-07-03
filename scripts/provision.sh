ff9288224e91882b394d8aa856d6f00ba61991a9
Alvaro Miranda Aguilera <kikitux@gmail.com>
2019-07-03 18:08:04 +0200

    refactor repeated code

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -5,6 +5,5 @@ mv /vagrant/conf/* /etc/consul.d/
 cat > /etc/consul.d/client.hcl <<EOF
 retry_join = ["${BUDDYIPADDR}"]
 bind_addr = "${IPADDR}"
-node_name = "${NODENAME}"
 EOF
 systemctl restart consul
