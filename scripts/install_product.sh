bb9765abdccaafafc28d127047c80b7af0a0da57
Krastin Krastev <krastin@hashicorp.com>
2019-07-05 09:12:39 +0200

    added cool intall_product script

---
+++ scripts/install_product.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# exit if product is not set
+if [ ! "$PRODUCT" ] ; then
+  echo "this script needs a variable PRODUCT=product"
+  exit 1
+fi
+
+VERSION=`curl -sL https://releases.hashicorp.com/${PRODUCT}/index.json | jq -r '.versions[].version' | sort -V | egrep -v 'ent|beta|rc|alpha' | tail -1`
+
+which ${PRODUCT} || {
+  cd /usr/local/bin
+  wget https://releases.hashicorp.com/${PRODUCT}/${VERSION}/${PRODUCT}_${VERSION}_linux_amd64.zip
+  unzip ${PRODUCT}_${VERSION}_linux_amd64.zip
+}
